89aee071bb713e45897fcf8c69802eb0b90a7466
Bram Van der Sype <bram.vandersype@gmail.com>
2024-10-08 09:51:54 +0200

    Brought over item list from previous PHP tool

---
+++ src/hooks/item.ts
@@ -0,0 +1,25 @@
+const items = [
+    'IRON_INGOT',
+    'COPPER_INGOT',
+    'CATERIUM_INGOT',
+    'RAW_QUARTZ',
+    'CONCRETE',
+    'RUBBER',
+    'SMART_PLATING',
+    'VERSATILE_FRAMEWORK',
+    'AUTOMATED_WIRING',
+    'HEAVY_MODULAR_FRAME',
+    'ENCASED_INDUSTRIAL_BEAM',
+    'MODULAR_FRAME',
+    'REINFORCED_IRON_PLATE',
+    'ROTOR',
+    'STATOR',
+    'MOTOR',
+    'AI_LIMITER',
+    'CRYSTAL_OSCILLATOR',
+    'CIRCUIT_BOARD',
+    'HIGH_SPEED_CONNECTOR',
+    'COMPUTER',
+] as const;
+
+export type Item = typeof items[number];
